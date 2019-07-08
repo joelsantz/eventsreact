9ba5d611359f40c505f64ccd05bb951f0219f217
Joel Santana <joelsantzdev@gmail.com>
2019-07-07 23:15:10 -0500

    modifed header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () =>  ( 
     <header className = "uk-margin" uk-margin = "true">
-        <h1 className = "uk-text-center">Events in React with Evenbrite API</h1>
+        <h1 className = "uk-text-center">Events in React with Eventbrite API</h1>
     </header>
  );
 
